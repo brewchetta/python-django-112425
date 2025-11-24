ab8b5b6c3bb45a731721a16e9377344f46a06483
Dan Rodney <dan@nobledesktop.com>
2025-11-24 16:18:06 -0500

    afternoon exercise

--- day-one-review/pages/urls.py
+++ day-one-review/pages/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("restaurant/<slug:name>", views.restaurant_details, name="restaurant_details")
 ]
+
+# 1. build two views for the app
+# 2. in one of the html pages use a for loop
+# 3. in one of the views use a dictionary in your context and grab that data to show on the html page
