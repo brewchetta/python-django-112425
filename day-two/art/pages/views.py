59b9fe71ad41bae3a753a5a9b2d40811e06e7f37
brewchetta <chett.tiller@gmail.com>
2025-11-25 16:48:21 -0500

    youtube embeds

--- day-two/art/pages/views.py
+++ day-two/art/pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {}
+    context = {
+        "video_ids": [
+            "jMJ6oF-4CcY?si=QZhrRtZgZi6Js25J",
+            "EPhWR4d3FJQ?si=Gup9VcEf8cm4nppy"
+        ]
+    }
     return render(request, 'pages/home.html', context)
